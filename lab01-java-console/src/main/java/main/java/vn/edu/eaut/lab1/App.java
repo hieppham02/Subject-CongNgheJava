95750d04002169188d77f06f618c4bb00b58d723
hieppham02 <hiepphamlc02@gmail.com>
2026-08-06 10:07:00 +0700

    update lab 1

--- lab01-java-console/src/main/java/main/java/vn/edu/eaut/lab1/App.java
+++ lab01-java-console/src/main/java/main/java/vn/edu/eaut/lab1/App.java
@@ -3,13 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package main.java.vn.edu.eaut.lab1;
-
+import java.util.*;
 /**
  *
  * @author edoga
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập n = ");
+        int n = scanner.nextInt();
+        if(KiemTraNguyenTo(n))
+            System.out.print(n + " la so nguyen to\n");
+        else{
+            System.out.print(n + " khong phai so nguyen to\n");
+        }
+        for(int i = 0; i <= n; i++){
+            System.out.print(Fibonaci(i) + " ");
+        }
+    }
+    
+    static public boolean KiemTraNguyenTo(int n){
+        if (n <= 2)
+            return false;
+        for(int i = 2; i*i <= n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    static int Fibonaci(int n){
+        if (n <= 1) return n;
+        return Fibonaci(n - 1) + Fibonaci(n - 2);
     }
 }
